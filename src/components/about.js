44e387ce2d4f12cc8b9431c7cc9368a848f334d3
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-12-16 13:47:02 -0500

    Setup about as clas component

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import styles from '../../style/about.css';
 
-export default () =>{
-  return <div> about daniel </div>
+export default class About extends Component{
+  componentDidUpdate(){
+    ReactDom.findDOMNode(this).scrollTop = 0;
+  }
+  render(){
+  return (
+      <div className={styles.about}> 
+        {
+        !this.props.doneOpen ? (<div>LOADING ....</div>) : 
+        (<div>
+          Content here
+        </div>)
+        }
+      </div>
+    );
+  }
 }
