4bb580618ecc23db212c279b5693b6aefc6a3341
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-12-16 09:39:43 -0500

    add info open to nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
 import styles from '../../style/navbar.css';
 import { Motion, spring } from 'react-motion';
-import NavMenu from './nav_menu';
+import NavMainMenu from './nav_menu';
 
 export default class Header extends Component {
   constructor(props){
     super(props);
 
-    this.state = { open: false}
+    this.state = { mainMenuOpen: false, infoOpen: false }
   }
 
   toggleMenu = () => {
-    console.log(this.state)
-    this.setState({ open: !this.state.open, render: true })
+    this.setState({ mainMenuOpen: !this.state.mainMenuOpen })
+  }
+  toggleInfo = () => {
+    this.setState({ infoOpen: !this.state.infoOpen })
   }
 
   render(){
-  const { open } = this.state;
+  const { mainMenuOpen, infoOpen } = this.state;
     return (
       <div>
           <div className={styles.closeX} onClick={this.toggleMenu} >
             <div className={styles.line1}></div>
             <div className={styles.line2}></div>
           </div>
+          <div onClick={this.toggleInfo} className={styles.info}>info</div>
         <div className={styles.navLeft}>
         </div>
-        <Motion style={{x: spring(open ? 100 : 0)}}>
-          {({x}) => 
+        <Motion style={{y: spring(mainMenuOpen ? 100 : 0)}}>
+          {({y}) => 
             <div className={styles.mainMenu} style={{
-                    WebkitTransform: `translate3d(0, ${x}%, 0)`,
-                    transform: `translate3d(0, ${x}%, 0)`,
+                    WebkitTransform: `translate3d(0, ${y}%, 0)`,
+                    transform: `translate3d(0, ${y}%, 0)`,
                   }}>
-              <NavMenu />
+              <NavMainMenu toggleMenu={this.toggleMenu}/>
+            </div>}
+        </Motion>
+        <Motion style={{x: spring(infoOpen ? -100 : 0)}}>
+          {({x}) => 
+            <div className={styles.infoMenu} style={{
+                    WebkitTransform: `translate3d(${x}%, 0, 0)`,
+                    transform: `translate3d(${x}%, 0, 0)`,
+                  }}>
             </div>}
         </Motion>
       </div> 
