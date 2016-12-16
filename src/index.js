d22a112dc6344661fc3043f0afbd52a3a3919445
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-12-16 09:39:23 -0500

    add routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
+import { Router, Route, browserHistory } from 'react-router';
 
-render(<App />, document.querySelector('.container') );
+import App from './components/app';
+import About from './components/about';
+import Projects from './components/projects';
+
+render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <Route path='/about' component={About}/>
+      <Route path='/projects' component={Projects}/>
+    </Route>
+
+  </Router>
+
+  , document.querySelector('.container') );
