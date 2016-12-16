9a3631ffc7fc7ec12b4c9f6d905248b907ed1f8c
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-12-16 13:47:45 -0500

    delete link to about me on main nav

--- src/components/nav_menu.js
+++ src/components/nav_menu.js
@@ -3,11 +3,9 @@ import styles from '../../style/navbar.css';
 import { Link } from 'react-router';
 
 export default (props) =>{
-  console.log(props)
   return(
       <div className={styles.navMainMenu}>
         <ul className={styles.menuUl}>
-          <li><Link onClick={props.toggleMenu} to='/about'>About Me</Link></li>
           <li><Link onClick={props.toggleMenu} to='/projects'>Projects</Link></li>
         </ul>
       </div>
