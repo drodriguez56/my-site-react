94e9b594c5f8e154b7b97df897ae945b78716c7a
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-13 12:39:40 -0500

    add list of project resources

---
+++ src/resources/projects.js
@@ -0,0 +1,37 @@
+export const PROJECTS = [
+  {
+    id: 1,
+    name: 'CLUNDA',
+    url: '/clunda',
+    imageUrl: '/assets',
+    skills: ['ruby','javascript']
+  },
+  {
+    id: 2,
+    name: 'LOTIFYME',
+    url: '/lotifyme',
+    imageUrl: './assets/lotifyme.png',
+    skills: ['ruby','javascript']
+  },
+    {
+    id: 3,
+    name: 'clunda',
+    url: '/clunda',
+    imageUrl: '/assets',
+    skills: ['ruby','javascript']
+  },
+  {
+    id: 4,
+    name: 'lotifyme',
+    url: '/lotifyme',
+    imageUrl: './assets/lotifyme.png',
+    skills: ['ruby','javascript']
+  },
+    {
+    id: 5,
+    name: 'clunda',
+    url: '/clunda',
+    imageUrl: '/assets',
+    skills: ['ruby','javascript']
+  }
+];
