b20f8a2fbc8a4a64f7398ff1be7148076af4c5f2
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-13 12:38:52 -0500

    extract loader logic to simple component

---
+++ src/components/loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../../style/loader.css';
+
+export default () =>{
+  return(
+        <div className={styles.loader}>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+    );
+}
