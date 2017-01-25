71476234cfdbfaf611d12abd14af9fc192dd1365
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-25 16:18:32 -0500

    delet logo from landing

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,8 +5,9 @@ export default () => {
   return (
     <div className={styles.landing}>
       <div className={styles.darckBack}></div>
-      <img src='https://s3.amazonaws.com/www.danielrodriguez.io/vendor/img/logo-personal.png'/> 
-      <h1>Full Stack Developer</h1>
+      <div className={styles.welcomeDiv}>
+        <h1 className={styles.homeH1} >Full Stack Developer</h1>
+      </div>
     </div>
   );
 }
