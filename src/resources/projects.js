360aac681793a6880e6765c6238af9c0365c5ea4
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-25 16:20:17 -0500

    placeholder url image projects

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -3,35 +3,42 @@ export const PROJECTS = [
     id: 1,
     name: 'CLUNDA',
     url: '/clunda',
-    imageUrl: '/assets',
-    skills: ['ruby','javascript']
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['Co-founder','Startup','Design' ]
   },
   {
     id: 2,
     name: 'LOTIFYME',
     url: '/lotifyme',
-    imageUrl: './assets/lotifyme.png',
-    skills: ['ruby','javascript']
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['Team Lead','RoR','API', 'IOS']
   },
     {
     id: 3,
-    name: 'clunda',
-    url: '/clunda',
-    imageUrl: '/assets',
-    skills: ['ruby','javascript']
+    name: 'PLATEANDO',
+    url: '/plateando',
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['SpreeCommerce','Project', 'Lead']
   },
   {
     id: 4,
-    name: 'lotifyme',
+    name: 'MRDESCUENTO',
     url: '/lotifyme',
-    imageUrl: './assets/lotifyme.png',
-    skills: ['ruby','javascript']
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['Co-founder','Startup']
   },
     {
     id: 5,
-    name: 'clunda',
+    name: 'EFUNDEX',
     url: '/clunda',
-    imageUrl: '/assets',
-    skills: ['ruby','javascript']
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['Wordpress','javascript', 'Manager']
+  },
+    {
+    id: 6,
+    name: 'SNIMPORT',
+    url: '/clunda',
+    imageUrl: 'http://angelforum-flensburg.de/gallery/images/gallerySmallPlaceholder.png',
+    skills: ['React.js','SpreeCommerce']
   }
 ];
