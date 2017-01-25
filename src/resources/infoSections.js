e755982d70dff41b5d19acc1e1f1096c84244c4e
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-25 13:29:14 -0500

    define sections content and image

---
+++ src/resources/infoSections.js
@@ -0,0 +1,14 @@
+export const  SECTIONS = [
+  {
+    imageUrl:'http://i164.photobucket.com/albums/u8/hemi1hemi/COLOR/COL9-6.jpg',
+    copy:'Daniel Rodriguez is an engineer Full Stack developer with experience in a variety of languages and frameworks.'
+  }, 
+  {
+    imageUrl:'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAeMAAAAJGJjNmQ1MTY2LWY5ZTAtNDU5Ni1iMjBkLTY3ZGJkYjc4OTcyMg.jpg',
+    copy:'other'
+  },
+  {
+    imageUrl:'http://linkbeef.com/wp-content/uploads/2016/03/FBWIJGYAKFZYJ4KD.jpeg',
+    copy:'other'
+  }
+];
