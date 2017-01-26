02fd209b96f255034421432e44e808c4a5f80222
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-26 15:18:08 -0500

    add not found route

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/app';
 import About from './components/about';
 import Projects from './components/projects';
-import Landing from './components/Landing';
+import Landing from './components/landing';
+import notFound from './components/notFound';
 
 render(
   <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Landing} />
       <Route path='/projects/:id' component={Projects}/>
+      <Route path='*' component={notFound} />
     </Route>
 
   </Router>
