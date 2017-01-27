df3f5cc6f19aceef8e91717d19d17b686f8f949e
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2017-01-26 21:09:02 -0500

    changes on projects info structure

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -27,15 +27,29 @@ export const PROJECTS = [
   {
     id: 4,
     name: 'MRDESCUENTO',
-    url: '/lotifyme',
+    url: '/mrdescuento',
     menuImageUrl: 'https://s3.amazonaws.com/www.danielrodriguez.io/images/mrdescuento.png',
     skills: ['Co-founder','Startup']
   },
     {
     id: 5,
+    disabled: true,
     name: 'SNIMPORT',
-    url: '/clunda',
     menuImageUrl: 'https://s3.amazonaws.com/www.danielrodriguez.io/images/snimport.png',
     skills: ['React.js','SpreeCommerce']
+  },
+    {
+    id: 6,
+    disabled: true,
+    name: 'BARROULETTE',
+    menuImageUrl: 'https://s3.amazonaws.com/www.danielrodriguez.io/images/GitHub-Mark.png',
+    skills: ['MongoDB','RoR', 'UberAPI']
+  },
+    {
+    id: 7,
+    disabled: true,
+    name: 'CONSUELA',
+    menuImageUrl: 'https://s3.amazonaws.com/www.danielrodriguez.io/images/GitHub-Mark.png',
+    skills: ['AlexaSkill','Amazon Lamda', 'Javascript']
   }
 ];
